6c47e8f7411123a10861364dc44be4036a9c1a15
AitBassouAli <aitbassouali@gmail.com>
2019-02-17 16:53:35 +0100

    add article component and service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ArticleComponent } from './components/article/article.component'
+const routes: Routes = [
+  {
+    path: 'auth', children: [
+      {
+        path: 'login', component: ArticleComponent
+      },
+      {
+        path: 'register', component: ArticleComponent
+      }
+    ]
+  },
+  {
+    path: 'notes', children: [
+      {
+        path: '', component: ArticleComponent
+      },
+      {
+        path: 'create', component: ArticleComponent
+      },
+      {
+        path: ':id', component: ArticleComponent
+      }
+    ]
+  },
+  {
+    path: 'upload', component: ArticleComponent
+  },
+  {
+    path: 'trash', component: ArticleComponent
+  },
+  { path: '', redirectTo: 'auth/login', pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article/article.component';
+import { ArticleService } from './sercices/article.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
